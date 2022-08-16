97a083b8838f1aac29a96f4820b75d01c92b50dc
ukorvl <ukorotovskiy@gmail.com>
2022-08-16 13:31:37 +0400

    access token replaced with reg token

--- action-runners/scripts/start.sh
+++ action-runners/scripts/start.sh
@@ -1,15 +1,8 @@
 #!/bin/bash
 
 ORGANIZATION=$ORGANIZATION
-ACCESS_TOKEN=$ACCESS_TOKEN
+REG_TOKEN=$REG_TOKEN
 RUNNER_LABEL=$RUNNER_LABEL
-
-# Get runner registration token
-REG_TOKEN=$(curl \
-  -X POST \
-  -H "Accept: application/vnd.github+json" \
-  -H "Authorization: token ${ACCESS_TOKEN}" \
-  https://api.github.com/orgs/${ORGANIZATION}/actions/runners/registration-token | jq .token --raw-output)
 
 # Configure runner
 cd /home/actions-runner
